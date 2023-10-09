4273d8b3f10cc95aa38cd12990508d3ae5bf43ea
nurmarief <ariefmuhammad897@gmail.com>
2023-10-09 20:06:02 +0700

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['@nurmarief/eslint-config', 'prettier'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          'webpack.common.js',
+          'webpack.dev.js',
+          'webpack.prod.js',
+        ],
+      },
+    ],
+  },
+};
